951d2e9d8fec6819711e2dda74d85052a7fe2183
Marcos Gerez <marcosmateogerez@gmail.com>
2025-10-11 11:22:26 -0300

    Ajuste de ruta.

--- src/web/controllers/login.py
+++ src/web/controllers/login.py
@@ -5,7 +5,7 @@ from werkzeug.exceptions import BadRequest
 bp_login = Blueprint("login", __name__, url_prefix="/login")
 
 
-@bp_login.post("/login")
+@bp_login.post("/")
 def authenticate():
     """
     Autentica al usuario y genera un token JWT.
